d89e3a61b79da09af60df0d23e11a2b8b548ed05
priyavatsya <43290628+priyavatsya@users.noreply.github.com>
2020-02-08 10:37:10 +0530

    Add files via upload

---
+++ 1st labinfy.py
@@ -0,0 +1,124 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> for i in range(61,123,1)
+SyntaxError: invalid syntax
+>>> for i n range(61,123,1):
+	
+SyntaxError: invalid syntax
+>>> for i in range(61,123,1):
+	print(chr(i))
+
+	
+=
+>
+?
+@
+A
+B
+C
+D
+E
+F
+G
+H
+I
+J
+K
+L
+M
+N
+O
+P
+Q
+R
+S
+T
+U
+V
+W
+X
+Y
+Z
+[
+\
+]
+^
+_
+`
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+m
+n
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
+>>> x=0
+>>> whilex<10:
+	
+SyntaxError: invalid syntax
+>>> x=0
+>>> while x<10:
+	x+=1
+	print(i)
+
+	
+122
+122
+122
+122
+122
+122
+122
+122
+122
+122
+>>> for i in range(1,11):
+	print(i)
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+>>> x=0
+>>> while x<10:
+	print(x)
+	x+=1
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
